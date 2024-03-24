8290460addb6ac0a21c6467d2293f9a6e7869f54
jaisal1311 <57360897+jaisal1311@users.noreply.github.com>
2024-03-24 16:31:35 +0530

    #auth0-integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { loginWithRedirect, logout, user, isAuthenticated, isLoading } =
+    useAuth0();
+
+  console.log({ user });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="banner">
+        {isLoading && <h1>Loading ...</h1>}
+        {!isAuthenticated ? (
+          <div>
+            <button className="button" onClick={() => loginWithRedirect()}>
+              Log In
+            </button>
+          </div>
+        ) : (
+          <div className="container ">
+            <div className="card">
+              <img src={user?.picture} alt={user?.name} />
+              <h2>{user?.name}</h2>
+              <p>{user?.email}</p>
+              <button
+                className="button"
+                onClick={() =>
+                  logout({ logoutParams: { returnTo: window.location.origin } })
+                }
+              >
+                Log Out
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-hxdubl7h6xo84yhl.us.auth0.com"
+      clientId="1iREZAjZDgQqtSdWtSFK73t7QX1TvUeR"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
